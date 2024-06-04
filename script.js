b5261dbe70f6a023c3b2ff8f911b52224943e679
Aziz-Mohamed <145936148+Aziz-Mohamed@users.noreply.github.com>
2024-06-04 12:47:41 +0300

    elements bundle stored in js successfully

--- script.js
+++ script.js
@@ -2,9 +2,28 @@ const apiKey = 'b1973fb700ef4db4b0b54352240406' ;
 const form = document.querySelector('.js-city-search');
 let searchbar = document.querySelector('js-city-Name');
 
+
 let cityEntered;
 let weatherData;
+
 let temperature;
+let country;
+let city;
+let conditionText;
+let conditionIcon;
+
+let temperatureElement = document.querySelector('.js-temperature span');
+let countryElement = document.querySelector('.js-country');
+let cityElement = document.querySelector('.js-city span ');
+let conditionTextElement = document.querySelector('.js-condition');
+let conditionIconElement = document.querySelector('.js-icon');
+
+temperatureElement = temperature;
+countryElement = country;
+cityElement = city;
+conditionTextElement = conditionText;
+conditionIconElement = conditionIcon;
+
 
 const getWeather = async (cityEntered) => {
   try{
@@ -13,17 +32,29 @@ const getWeather = async (cityEntered) => {
       throw new Error(`API request failed with status ${response.status}`);
     }
     weatherData = await response.json();
-    console.log(weatherData);
+
+    temperature = weatherData.current.temp_c;
+    country = weatherData.location.country;
+    city = weatherData.location.region;
+    conditionText = weatherData.current.condition.text;
+    conditionIcon = weatherData.current.condition.icon;
+
+    temperatureElement = temperature;
+    countryElement = country;
+    cityElement = city;
+    conditionTextElement = conditionText;
+    conditionIconElement = conditionIcon;
+    // checkFunction();
   }catch (e) {
     console.log(e.name)
   }
 };
 
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   cityEntered = document.querySelector('.js-city-Name').value;
   if (cityEntered) { 
-    console.log(cityEntered);
     getWeather(cityEntered);
   } else {
     console.error("Please enter a city name");
@@ -31,4 +62,17 @@ form.addEventListener("submit", (e) => {
 })
 
 
+const checkFunction = () => {
+console.log("$$Temperature:", temperature);
+console.log("$$Country:", country);
+console.log("$$City:", city);
+console.log("$$Condition Text:", conditionText);
+console.log("$$Condition Icon URL:", conditionIcon);
 
+console.log("Temperature:", temperatureElement);
+console.log("Country:", countryElement);
+console.log("City:", cityElement);
+console.log("Condition Text:", conditionTextElement);
+console.log("Condition Icon URL:", conditionIconElement);
+}
+// checkFunction();
